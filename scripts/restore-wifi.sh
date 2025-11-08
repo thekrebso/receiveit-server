28d8a6e2813f02a1c4d5c90956eba9682c48862e
krebso <krebso@krebso.pl>
2025-11-08 23:37:28 +0100

    also stop autoconnect on restore wifi

--- scripts/restore-wifi.sh
+++ scripts/restore-wifi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+systemctl stop p2p-autoconnect.path
+systemctl stop p2p-autoconnect.service
 systemctl stop p2p-events.path
 systemctl stop p2p-events.service
 systemctl stop p2p-ifwatch.service
