cebbeac30cc53eb7cf0db77be2c0d4d2cc84f4ce
krebso <krebso@krebso.pl>
2025-11-08 22:09:07 +0100

    change to p2p-dev-wlan0

--- scripts/p2p-find.sh
+++ scripts/p2p-find.sh
@@ -2,7 +2,7 @@
 FIND_INTERVAL=30
 
 while true; do
-	echo "[p2p-find] Running p2p_find on wlan0"
-	sudo wpa_cli -i wlan0 p2p_find >/dev/null 2>&1
+	echo "[p2p-find] Running p2p_find on p2p-dev-wlan0"
+	sudo wpa_cli -i p2p-dev-wlan0 p2p_find >/dev/null 2>&1
 	sleep $FIND_INTERVAL
 done
