0574a71a64ffcc4ad065fa0b9ae012071b7fe199
krebso <krebso@krebso.pl>
2025-11-08 21:24:05 +0100

    recursively

--- scripts/update-files.sh
+++ scripts/update-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /home/receiveit/receiveit-server || exit 1
-cp ./etc/systemd/system/* /etc/systemd/system/
+cp -r ./etc/systemd/system/* /etc/systemd/system/
 cp ./etc/wpa_supplicant/* /etc/wpa_supplicant/
 cp ./boot/firmware/* /boot/firmware/
